0ea26ba6bc68b4c7e49c7dcdcde6e0384614dbf6
Daniel Meechan <dm280@st-andrews.ac.uk>
2018-11-03 15:30:25 +0000

    Added env check in credentials

--- server/credentials.js
+++ server/credentials.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 
+const environment = process.env.NODE_ENV;
+console.log(`Running in environment: ${environment}`);
+
 if (!process.env.DB_USER) {
     console.warn('Whoops, could not find DB_USER in environment variables. Make sure you have them all set!');
+}
+
+if (!process.env.AWS_ACCESS_KEY) {
+    console.warn('Whoops, could not find AWS_ACCESS_KEY in environment variables. Make sure you have them all set!');
 }
 
 module.exports = {
